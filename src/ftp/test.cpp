00612dbff33e4e0bbe36f192f19484292b98d373
Nils Henrich <nilshenrich@users.noreply.github.com>
2025-01-18 20:31:56 +0100

    It is not allowed to link an existing stream in ostream creator method. If a new stream instance is created, the program runs properly

--- src/ftp/test.cpp
+++ src/ftp/test.cpp
@@ -11,10 +11,22 @@ using namespace ::tcp;
 using namespace ::ftp;
 using namespace ::std::chrono_literals;
 
+class MyOstream : public ostream
+{
+public:
+    MyOstream() {}
+    ~MyOstream() {}
+    template <typename T>
+    MyOstream &operator<<(const T &value)
+    {
+        cout << "MyOstream says '" << value << "'" << endl;
+        return *this;
+    }
+};
+
 int main()
 {
     FtpServer server;
-    istringstream iFile{"My file content"};
     server.setWork_checkUserCredentials([](const string, const string) -> bool
                                         { return true; });
     server.setWork_checkAccessible([](const string, const string) -> bool
@@ -25,10 +37,10 @@ int main()
                                                          Item{ItemType::file, "MyFile", {6, 4, 4}, 0, 10, 11, 4096, 1722164144}}; });
     server.setWork_createDirectory([](const string) -> bool
                                    { return true; });
-    server.setWork_readFile([&iFile](const string) -> istringstream *
-                            { return &iFile; });
-    server.setWork_writeFile([](const string) -> ostream *
-                             { return &cout; });
+    server.setWork_readFile([](const string path) -> istringstream *
+                            { return new istringstream{"My file content for file '"s + path + "'"s}; });
+    server.setWork_writeFile([](const string path) -> MyOstream *
+                             { return new MyOstream(); });
     if (server.start())
         return -1;
     this_thread::sleep_for(5min);
