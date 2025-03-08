211f7947036430e573bc7f6a88a56b0126cad94f
Nils Henrich <nilshenrich@users.noreply.github.com>
2025-03-08 09:24:46 +0100

    <hook> Update to version 3.2.1

--- src/TcpServer.hpp
+++ src/TcpServer.hpp
@@ -2,10 +2,10 @@
  * @file TcpServer.hpp
  * @author Nils Henrich
  * @brief TCP server for unencrypted data transfer without authentication.
- * @version 3.2.0
+ * @version 3.2.1
  * @date 2021-12-27
  *
- * @copyright Copyright (c) 2021
+ * @copyright Copyright (c) 2025
  *
  */
 
