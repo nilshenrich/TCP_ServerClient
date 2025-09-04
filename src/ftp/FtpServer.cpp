c7c8dc61c48f0aa2a32a07999159af5966636477
Nils Henrich <nilshenrich@users.noreply.github.com>
2025-09-04 18:09:04 +0200

    Add bug tag. Using an ofstream to redirect an incoming stream into a file doesn't work for small files that don't exceed the dynamic buffer

--- src/ftp/FtpServer.cpp
+++ src/ftp/FtpServer.cpp
@@ -649,6 +649,7 @@ void FtpServer::on_msg_fileUpload(const int clientId, const uint32_t command, co
     }
 
     // Get stream to file that should be uploaded and redirect data server output to file stream
+    // BUG: [ofstream] Works well for big files, but small files stay empty (when buffer size not reached)
     path += "/"s + args[0];
     incomingStreamFwd->redirect(work_writeFile(path, getStreamOpenMode(STREAM_DIRECTION_WRITE, transferType)));
 
